01370b4d237573a2b6c0b77a0a4f8c8e0d2f0f8f
Pankaj Kumar Verma <pankajpatel916174@gmail.com>
2023-04-06 19:31:19 +0530

    adding js features

--- js/app.js
+++ js/app.js
@@ -3,23 +3,28 @@ const deleteNotes = document.getElementById("delete-notes");
 const saveNotes = document.getElementById("save-notes");
 const textareaNotes = document.getElementById("text-area");
 const container = document.querySelector('.container');
-addNotes.addEventListener('click', 
-    function(){
+addNotes.addEventListener('click',
+    function () {
         addNote();
     }
 )
-const saveNote = () =>{
+const saveNote = () => {
     const NotesData = document.querySelectorAll("#text-area");
-    console.log(NotesData)
     const data = [];
     NotesData.forEach(
         (note) => data.push(note.value)
     )
     // console.log(data);
-    
+    if (data.length === 0) {
+        localStorage.removeItem('note')
+    } else {
+        localStorage.setItem('note', JSON.stringify(data))
+    }
+    localStorage.setItem('note', JSON.stringify(data))
+
 }
 
-const addNote = () =>{
+const addNote = (text = "") => {
     const note = document.createElement("div");
     note.classList.add('note');
     note.innerHTML = `
@@ -29,20 +34,48 @@ const addNote = () =>{
         <i class="fa-solid fa-trash delete-icon" id="delete-notes"></i>
     </div>
     <div class="textarea">
-        <textarea name="" id="text-area" placeholder="Write your note here!"></textarea>
+        <textarea name="" id="text-area" placeholder="Write your note here!">${text}</textarea>
     </div>
 </div>
 `;
 
     note.querySelector('.delete-icon').addEventListener('click',
-        function(){
+        function () {
             note.remove();
-        } 
+            saveNote();
+        }
     )
     note.querySelector('.save-icon').addEventListener('click',
+        function () {
+            saveNote();
+        }
+    )
+    note.querySelector('textarea').addEventListener(
+        'focusout',
         function(){
             saveNote();
         }
     )
     container.appendChild(note);
+    saveNote();
 }
+(
+    function () {
+        const lsnote = JSON.parse(localStorage.getItem('note'));
+
+        if (lsnote === null) {           
+                    addNote();              
+        } else {
+            lsnote.forEach(
+                (lsnote) => {
+                    addNote(lsnote);
+                }
+            )
+        }
+        // if (lsnote.length === 0) {
+        //     localStorage.removeItem('note')
+        // } else {
+        //     addNote();
+        // }
+    }
+)()
